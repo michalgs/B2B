e8071f6f48907f6f47371ca8bb0c742e539d483d
Bartosz Wrona <EasyRider190@gmail.com>
2026-04-14 15:04:36 +0200

    limiting cypress resources

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'stock') {
+          launchOptions.args.push('--disable-dev-shm-usage');
+          launchOptions.args.push('--disable-gpu');
+          launchOptions.args.push('--no-sandbox');
+          return launchOptions;
+        }
+      });
+    },
+
     baseUrl: "http://localhost:3001",
     screenshotsFolder: "cypress/screenshots",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     supportFile: "cypress/support/e2e.ts",
   },
 });
